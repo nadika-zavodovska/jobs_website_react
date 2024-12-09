bd53af4b2f2452dd46c7945e0aeab5b6ac2e4bea
nadika <nadikazavodovska@gmail.com>
2024-12-09 09:40:02 +0000

    Add components on home page

--- jobs-website-react/src/App.jsx
+++ jobs-website-react/src/App.jsx
@@ -1,7 +1,12 @@
 import { Route, createBrowserRouter, createRoutesFromElements, RouterProvider } from 'react-router-dom';
+import MainLayout from './layouts/MainLayout';
 import HomePage from './pages/HomePage';
 
-const router = createBrowserRouter(createRoutesFromElements(<Route index element={<HomePage />} />));
+const router = createBrowserRouter(createRoutesFromElements(
+  <Route path='/' element={<MainLayout />}>
+    <Route index element={<HomePage />} />
+  </Route>
+));
 
 const App = () => {
   return <RouterProvider router={router} />;
