2f5a24d9ab70ca5869673d9890e8d9793286941b
nadika <nadikazavodovska@gmail.com>
2024-12-09 14:55:21 +0000

    Implement jobpage

--- jobs-website-react/src/App.jsx
+++ jobs-website-react/src/App.jsx
@@ -2,13 +2,15 @@ import { Route, createBrowserRouter, createRoutesFromElements, RouterProvider } 
 import MainLayout from './layouts/MainLayout';
 import HomePage from './pages/HomePage';
 import JobsPage from './pages/JobsPage';
-import NotFoundPage from './pages/NotFound'
+import NotFoundPage from './pages/NotFound';
+import JobPage, { jobLoader } from './pages/JobPage';
 
 const router = createBrowserRouter(createRoutesFromElements(
   <Route path='/' element={<MainLayout />}>
     <Route index element={<HomePage />} />
     <Route path='/jobs' element={<JobsPage />} />
-    <Route path='/*' element={<NotFoundPage />} />
+    <Route path='/jobs/:id' element={<JobPage />} loader={jobLoader} />
+    <Route path='/*' element={<NotFoundPage />} />    
   </Route>
 ));
 
