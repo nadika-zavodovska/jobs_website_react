0ba58fcdae45991acb6c4ce03bb46ccb452df737
nadika <nadikazavodovska@gmail.com>
2024-12-09 10:33:58 +0000

    Add notfound page

--- jobs-website-react/src/App.jsx
+++ jobs-website-react/src/App.jsx
@@ -2,11 +2,13 @@ import { Route, createBrowserRouter, createRoutesFromElements, RouterProvider } 
 import MainLayout from './layouts/MainLayout';
 import HomePage from './pages/HomePage';
 import JobsPage from './pages/JobsPage';
+import NotFoundPage from './pages/NotFound'
 
 const router = createBrowserRouter(createRoutesFromElements(
   <Route path='/' element={<MainLayout />}>
     <Route index element={<HomePage />} />
     <Route path='/jobs' element={<JobsPage />} />
+    <Route path='/*' element={<NotFoundPage />} />
   </Route>
 ));
 
